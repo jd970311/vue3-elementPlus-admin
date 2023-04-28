9bdaa57776e56971a7d5630610ba1a6065589919
JD <jd970311@163.com>
2023-04-28 17:46:36 +0800

    用户退出被动处理

--- src/utils/request.js
+++ src/utils/request.js
@@ -9,58 +9,6 @@ const service = axios.create({
 })
 
 // 请求拦截器
-// service.interceptors.request.use(
-//   (config) => {
-//     const { icode, time } = getTestICode()
-//     config.headers.icode = icode
-//     config.headers.codeType = time
-//     // 在这个位置需要统一的去注入token
-//     if (store.getters.token) {
-//       // 如果token存在 注入token
-//       config.headers.Authorization = `Bearer ${store.getters.token}`
-//       if (isCheckTimeout()) {
-//         // 登出操作
-//         store.dispatch('user/logout')
-//         return Promise.reject(new Error('token 失效'))
-//       }
-//     }
-//     // 配置接口国际化
-//     config.headers['Accept-Language'] = store.getters.language
-//     return config // 必须返回配置
-//   },
-//   (error) => {
-//     return Promise.reject(error)
-//   }
-// )
-
-// 响应拦截器
-// service.interceptors.response.use(
-//   (response) => {
-//     const { success, message, data } = response.data
-//     //   要根据success的成功与否决定下面的操作
-//     if (success) {
-//       // 成功返回解析后的数据
-//       return data
-//     } else {
-//       // 业务错误
-//       ElMessage.error(message) // 提示错误消息
-//       return Promise.reject(new Error(message))
-//     }
-//   },
-//   (error) => {
-//     // 处理 token 超时问题
-//     if (
-//       error.response &&
-//       error.response.data &&
-//       error.response.data.code === 401
-//     ) {
-//       // token超时
-//       store.dispatch('user/logout')
-//     }
-//     ElMessage.error(error.message) // 提示错误信息
-//     return Promise.reject(error)
-//   }
-// )
 service.interceptors.request.use(config => {
   const { icode, time } = getTestICode()
   config.headers.icode = icode
@@ -80,6 +28,8 @@ service.interceptors.request.use(config => {
   // 对请求错误做些什么
   return Promise.reject(error);
 })
+
+// 请求响应器
 service.interceptors.response.use(function (response) {
   // 2xx 范围内的状态码都会触发该函数。
   // 对响应数据做点什么
@@ -95,6 +45,10 @@ service.interceptors.response.use(function (response) {
 }, function (error) {
   // 超出 2xx 范围的状态码都会触发该函数。
   // 对响应错误做点什么
+  // 被动处理 token 超时问题
+  if (error.response && error.response.data && error.response.data.code === 401) {
+    store.dispatch('user/logout')
+  }
   ElMessage.error(error.message)
   return Promise.reject(error);
 });
